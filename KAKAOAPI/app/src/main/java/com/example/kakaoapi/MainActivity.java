39fa7cd72c28719ad7b2ac227dad88c938396d7b
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2021-11-11 15:09:12 +0900

    서근재
    KAKAO API Test

---
+++ KAKAOAPI/app/src/main/java/com/example/kakaoapi/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.kakaoapi;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+
+    }
+
+
+
+}
