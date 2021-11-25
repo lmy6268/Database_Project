0185042e3fd345b25c95e718931cfbca81d6324f
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2021-11-25 19:08:52 +0900

    Kakao map 테스트
    테스트

--- KAKAOAPI/app/src/main/java/com/example/kakaoapi/MainActivity.java
+++ KAKAOAPI/app/src/main/java/com/example/kakaoapi/MainActivity.java
@@ -1,22 +1,35 @@
 package com.example.kakaoapi;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
-import android.view.ViewGroup;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
 
 import net.daum.mf.map.api.MapView;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.text.BreakIterator;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_location;
+    Button btn_myLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +42,100 @@ public class MainActivity extends AppCompatActivity {
         mapViewContainer.addView(mapView);
 
 
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permission == PackageManager.PERMISSION_DENIED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION
+                        },
+                        1000);
+            }
+            return;
+        }
+        tv_location = findViewById(R.id.tv_location);
+        btn_myLocation = findViewById(R.id.btn_myLocation);
+        btn_myLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLocationService();
+            }
+        });
+    }
+    public void startLocationService() {
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+           // showCurrentLocation(latitude, longitude);
+            Toast.makeText(getApplicationContext(), String.format("%lf,%lf",latitude,longitude), Toast.LENGTH_SHORT).show();
+
+        }else{
+            Toast.makeText(getApplicationContext(), "위치를 알 수 없습니다.", Toast.LENGTH_SHORT).show();
+        }
+        GPSListener gpsListener = new GPSListener();
+        long minTime = 10000;
+        float minDistance = 0;
+
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, gpsListener);
+        Toast.makeText(getApplicationContext(), "내 위치확인 요청함", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grandResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grandResults);
+        if (requestCode == 1000) {
+            boolean check_result = true;
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+            if (check_result == true) {
+            } else {
+                finish();
+            }
+        }
     }
 
 
 
 }
+
+class GPSListener implements LocationListener {
+    @Override
+    public void onLocationChanged(Location location) {
+        Double latitude = location.getLatitude();
+        Double longitude = location.getLongitude();
+        System.out.println(latitude +" "+ longitude);
+        //showCurrentLocation(latitude, longitude);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    private void showCurrentLocation(Double latitude, Double longitude) {
+ //       LatLng curPoint = new LatLng(latitude, longitude);
+//     map.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 15));
+
+    }
+
+
+
+
+}
+
