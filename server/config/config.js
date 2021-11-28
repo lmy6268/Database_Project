68fb74366b422ee9ae6ce4a0ea7739926dcba07c
lmy6268 <70847610+lmy6268@users.noreply.github.com>
2021-11-29 04:20:05 +0900

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config();
+const path = require('path');
+dotenv.config({ path: __dirname + "/../.env" });
 
 module.exports = { 
     development: {
       username: "skudbproject2021",  //db의 사용자명 입력
-      password: process.env.password,  //db의 비밀번호
+      password: process.env.PASSWORD,  //db의 비밀번호
       database: "sampleDB", 
       host: "193.122.126.186",
       dialect: "mysql",
       operatorAliases : false
     }
   }
-
